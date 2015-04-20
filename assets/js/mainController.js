13c0a43874cf7d404478bda18e756db26a29080b
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2015-04-20 18:33:26 +0700

    add rating for skill

--- assets/js/mainController.js
+++ assets/js/mainController.js
@@ -49,7 +49,8 @@ $(function(){
     var msg='';
     if (reason==='unsupported-file-type') { msg = "Unsupported format " +detail; }
     else {
-      console.log("error uploading file", reason, detail);
+      //console.log("error uploading file", reason, detail);
+      alert("error uploading file", reason, detail);
     }
     $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>'+ 
      '<strong>File upload error</strong> '+msg+' </div>').prependTo('#alerts');
